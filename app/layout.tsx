e38b1e4e34073a3e7140a7e3b315a165ad4f6d40
francanete <francanetedev@gmail.com>
2026-02-08 19:54:05 +0000

    Add plaudera

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Script
+          id="plaudera-widget"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d){var p=w.Plaudera=w.Plaudera||function(){(p.q=p.q||[]).push(arguments)};p.l=+new Date;var s=d.createElement("script");s.async=1;s.src="http://localhost:3000/widget.js";d.head.appendChild(s)}(window,document);Plaudera('init',{workspace:'i3i8l3qfgp0h1zxe2n0dcswj'});`,
+          }}
+        />
       </body>
     </html>
   );
---
+++ app/pollas/page.tsx
@@ -0,0 +1,9 @@
+const PollasPage = () => {
+  return (
+    <div>
+      <h1>Pollas Technologies</h1>
+    </div>
+  );
+};
+
+export default PollasPage;
