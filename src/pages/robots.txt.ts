4e021f9d7c8d902042825a64d0864faab0ac2f5a
francanete <francanetedev@gmail.com>
2026-06-21 20:38:51 +0100

    Fix GEO

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,7 +3,7 @@ import { siteUrl } from '../lib/site';
 export async function GET() {
   const body = `User-agent: *
 Allow: /
-Sitemap: ${siteUrl}/sitemap-index.xml`;
+Sitemap: ${siteUrl}/sitemap.xml`;
   return new Response(body, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
   });
